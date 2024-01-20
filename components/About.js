254d5c3489c248472ad6874532f7e8e0e9e91eaf
Md Misbah <80062894+md-misbah@users.noreply.github.com>
2024-01-20 10:27:41 +0600

    Update About.js

--- components/About.js
+++ components/About.js
@@ -14,7 +14,7 @@ export default function About() {
             <div className="sm:container mx-auto">
                 <div className="aboutWrapper w-11/12 m-auto">
                     <div className="aboutHeadingTitle mb-12">
-                        <h3 className='subTitle mx-auto text-center text-sm textAlt mb-1.5'>My Intro</h3>
+                        <h3 className='subTitle mx-auto text-center text-sm text-white mb-1.5'>My Intro</h3>
                         <h1 className="headTitleLine mx-auto text-2xl text-center custom font-semibold">About Me</h1>
                     </div>
                     <div className="aboutDataWrapper grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-x-0 gap-y-8">
@@ -30,31 +30,31 @@ export default function About() {
                         </div>
                         <div className="aboutmetaWrap">
                             <div className="aboutBox grid grid-cols-3 gap-x-4 mb-12">
-                                <div className="aboutIcoBox text-center py-4 px-5 customShadow csBg rounded-xl">
+                                <div className="aboutIcoBox text-center py-4 px-5 shadow csBg rounded-xl">
                                     <div className="abIco my-2">
                                         <FaAward className="customLime m-auto text-xl" />
                                     </div>
-                                    <h1 className="mt-2 mb-0 text-sm font-semibold textMain">Experience</h1>
-                                    <span className="mb-2 tinyFont textAlt text-xs">3++ Years Working</span>
+                                    <h1 className="mt-2 mb-0 text-sm font-semibold text-white">Experience</h1>
+                                    <span className="mb-2 tinyFont text-white text-xs">3++ Years Working</span>
                                 </div>
-                                <div className="aboutIcoBox text-center py-4 px-4 customShadow csBg rounded-xl">
+                                <div className="aboutIcoBox text-center py-4 px-4 shadow csBg rounded-xl">
                                     <div className="abIco my-2">
                                         <ImBriefcase className="customLime m-auto text-xl" />
                                     </div>
-                                    <h1 className="mt-2 mb-0 text-sm font-semibold textMain">Completed</h1>
-                                    <span className="mb-2 tinyFont textAlt text-xs">30++ Projects</span>
+                                    <h1 className="mt-2 mb-0 text-sm font-semibold text-white">Completed</h1>
+                                    <span className="mb-2 tinyFont text-white text-xs">30++ Projects</span>
                                 </div>
-                                <div className="aboutIcoBox text-center py-4 px-5 customShadow csBg rounded-xl">
+                                <div className="aboutIcoBox text-center py-4 px-5 shadow csBg rounded-xl">
                                     <div className="abIco my-2">
                                         <BiSupport className="customLime m-auto text-xl" />
                                     </div>
-                                    <h1 className="mt-2 mb-0 text-sm font-semibold textMain">Support</h1>
-                                    <span className="mb-2 tinyFont textAlt text-xs">Online 24/7</span>
+                                    <h1 className="mt-2 mb-0 text-sm font-semibold text-white">Support</h1>
+                                    <span className="mb-2 tinyFont text-white text-xs">Online 24/7</span>
                                 </div>
 
                             </div>
                             <div className="aboutHeadTitle"><h1 className='text-2xl text-left mb-3 mt-8 customLime font-semibold'> Md Misbah </h1></div>
-                            <div className="aboutdesc mb-4"><p className='text-justify text-base textAlt'>I'm a multi-skilled professional, who dominates the fields of full-stack web programming, data wizardry, graphic design, virtual assisting, and project management. I'm all about the tech and creativity fusion, bringing a laid-back yet meticulous vibe to my work. Let's turn ideas into digital awesomeness!</p></div>
+                            <div className="aboutdesc mb-4"><p className='text-justify text-base text-white'>I'm a multi-skilled professional, who dominates the fields of full-stack web programming, data wizardry, graphic design, virtual assisting, and project management. I'm all about the tech and creativity fusion, bringing a laid-back yet meticulous vibe to my work. Let's turn ideas into digital awesomeness!</p></div>
 
                             <div className="aboutButton mt-8 flex justify-around">
                                 <button className="group relative h-12 w-40 overflow-hidden rounded-lg bg-white text-lg shadow">
